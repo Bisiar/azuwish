5837263e9d66485354cf1c64eb4709bb4fb9dbfa
James Bisiar <Bisiar@gmail.com>
2023-06-08 10:07:25 -0400

    test deploy

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,6 +1,39 @@
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+
 namespace BlazorBasic.Pages;
 
-public class Index_razor
+partial class Index
 {
     
+    List<Resource>? resources;
+    string? selectedCustomerId;
+    string? selectedCustomerName;
+    string? filter;
+ 
+    
+    async Task HandleInput(ChangeEventArgs e)
+    {
+        filter = e.Value?.ToString();
+        if (filter?.Length > 2)
+        {
+            //resources = await http.GetFromJsonAsync<List<Resource>>($"/data/companyfilter?filter={filter}");
+        }
+        else
+        {
+            resources = null;
+            selectedCustomerName = selectedCustomerId = null;
+        }
+    }
+
+    private Task SelectResource(object resourceId)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class Resource
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
 }
